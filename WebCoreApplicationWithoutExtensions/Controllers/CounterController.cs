5eb284aec386765c46899a0008795fc585e52531
Sergey Kuznetsov <sergey.kuznetsov@hotmail.com>
2017-12-05 18:44:29 +0300

    init

---
+++ WebCoreApplicationWithoutExtensions/Controllers/CounterController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Prometheus.Client;
+
+namespace WebCoreApplicationWithoutExtensions.Controllers
+{
+    [Route("[controller]")]
+    public class CounterController : Controller
+    {
+        readonly Counter _counter = Metrics.CreateCounter("myCounter", "some help about this");
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+            _counter.Inc();
+            return Ok();
+        }
+    }
+}
