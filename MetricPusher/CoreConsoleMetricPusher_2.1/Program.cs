13ec97d785b177a5768deb7af6a6d59b5faa41b0
Sergey Kuznetsov <sergey.kuznetsov@hotmail.com>
2018-07-05 01:56:56 +0300

    add MetricPusher example

---
+++ MetricPusher/CoreConsoleMetricPusher_2.1/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Prometheus.Client.MetricPusher;
+
+namespace CoreConsoleMetricPusher_2._1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MetricPusherWorker.Start();
+            Console.WriteLine("Hello World!");
+
+            while (true)
+            {
+                
+            }
+        }
+    }
+}
