9c6959bc67861169599ff8d06e0b324f963cdb61
Sergey Kuznetsov <sergey.kuznetsov@hotmail.com>
2018-07-08 01:24:40 +0300

    MetricPusher example

--- MetricPusher/CoreConsoleMetricPusher_2.1/Program.cs
+++ MetricPusher/CoreConsoleMetricPusher_2.1/Program.cs
@@ -1,15 +1,39 @@
 ﻿using System;
+using System.Threading;
+using Prometheus.Client;
+using Prometheus.Client.Collectors;
+using Prometheus.Client.MetricPusher;
 
 namespace CoreConsoleMetricPusher_2._1
 {
-    class Program
+    internal class Program
     {
-        static void Main(string[] args)
-        {
+        internal static void Main(string[] args)
+        {                     
+            var defaultPusher = new MetricPusher("http://localhost:9091", "pushgateway-testworker", "default");
+            var registry = new CollectorRegistry();
+            var customPusher = new MetricPusher(registry, "http://localhost:9091", "pushgateway-testworker", "custom");
             
-            Console.WriteLine("Hello World!");
+            var counter = Metrics.CreateCounter("example_counter1", "help");
+            var counterInCustom =  Metrics.WithCustomRegistry(registry).CreateCounter("example_counter2", "help1");
+            
+            IMetricPushServer server = new MetricPushServer(new IMetricPusher[]
+            {
+                defaultPusher, customPusher
+            });
+            
+            server.Start();
 
+            for (int i = 0; i < 10; i++)
+            {
+                counter.Inc();
+                counterInCustom.Inc(2);
+                
+                Console.WriteLine("count: " + i);
+                Thread.Sleep(2000);
+            }
             
+            server.Stop();
         }
     }
 }
