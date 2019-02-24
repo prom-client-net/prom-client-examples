dcef404e36631bbe5a677b7dc2842852de496af0
Sergey Kuznetsov <sergey.kuznetsov@hotmail.com>
2019-02-24 22:07:58 +0300

    Update example

--- WithoutExtensions/CoreWebWithoutExtensions_2.1/Startup.cs
+++ WithoutExtensions/CoreWebWithoutExtensions_2.1/Startup.cs
@@ -35,9 +35,6 @@ namespace CoreWebWithoutExtensions
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
         {
-            loggerFactory.AddConsole(Configuration.GetSection("Logging"));
-            loggerFactory.AddDebug();
-
             app.UseMvc();
         }
     }
