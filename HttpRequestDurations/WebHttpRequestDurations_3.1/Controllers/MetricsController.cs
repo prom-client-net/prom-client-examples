979be3ea42c232b7a0658c77544389d6147c271d
Sergey Kuznetsov <cmt@phnx47.net>
2020-01-17 04:55:51 +0700

    Add Examples

---
+++ HttpRequestDurations/WebHttpRequestDurations_3.1/Controllers/MetricsController.cs
@@ -0,0 +1,18 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Prometheus.Client;
+
+namespace CoreWebWithoutExtensions_3._1.Controllers
+{
+    [Route("[controller]")]
+    public class MetricsController : Controller
+    {
+        [HttpGet]
+        public async Task Get()
+        {
+            Response.StatusCode = 200;
+            await using var outputStream = Response.Body;
+            await ScrapeHandler.ProcessAsync(Metrics.DefaultCollectorRegistry, outputStream);
+        }
+    }
+}
---
+++ HttpRequestDurations/WebHttpRequestDurations_3.1/Startup.cs
@@ -0,0 +1,71 @@
+using System;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Prometheus.Client.AspNetCore;
+using Prometheus.Client.HttpRequestDurations;
+
+namespace CoreWebWithoutExtensions_3._1
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddControllers();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            app.UseRouting();
+            
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+            
+            app.UsePrometheusServer(q => q.UseDefaultCollectors = false);
+            app.UsePrometheusRequestDurations(q =>
+            {
+                q.IncludePath = true;
+                q.IncludeMethod = true;
+                q.IgnoreRoutesConcrete = new[]
+                {
+                    "/favicon.ico",
+                    "/robots.txt",
+                    "/"
+                };
+                q.IgnoreRoutesStartWith = new[]
+                {
+                    "/swagger"
+                };
+                q.CustomNormalizePath = new Dictionary<Regex, string>
+                {
+                    { new Regex(@"\/[0-9]{1,}(?![a-z])"), "/id" }
+                };
+
+                // Just for example. Not for Production
+                q.CustomLabels = new Dictionary<string, Func<string>>
+                {
+                    {
+                        "application_name", () =>  env.ApplicationName
+                    },
+                    {
+                        "date", () => DateTime.UtcNow.ToString("yyyy-MM-dd")
+                    }
+                };
+            });
+        }
+    }
+}
