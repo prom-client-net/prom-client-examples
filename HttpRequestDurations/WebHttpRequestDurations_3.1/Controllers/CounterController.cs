be46062b445bedbb62e43114ecef922f0267c114
Serge K <cmt@phnx47.net>
2020-08-24 00:29:43 +0700

    counter label

--- HttpRequestDurations/WebHttpRequestDurations_3.1/Controllers/CounterController.cs
+++ HttpRequestDurations/WebHttpRequestDurations_3.1/Controllers/CounterController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using System;
+using Microsoft.AspNetCore.Mvc;
 using Prometheus.Client;
 using Prometheus.Client.Abstractions;
 
@@ -9,19 +10,21 @@ namespace CoreWebWithoutExtensions_3._1.Controllers
     {
         private readonly ICounter _counter;
         private readonly ICounter _counterTs;
+        private readonly IMetricFamily<ICounter, ValueTuple<string, string>> _conterLabel;
         
         public CounterController(IMetricFactory metricFactory)
         {
             _counter =  metricFactory.CreateCounter("my_counter", "some help about this");
+            _conterLabel = metricFactory.CreateCounter("my_counter_label", "some help about this", ("label1","label2"));
             _counterTs = metricFactory.CreateCounter("my_counter_ts", "some help about this", true);
         }
         
         [HttpGet]
         public IActionResult Get()
-        {
+        { 
             _counter.Inc();
             _counterTs.Inc(3);
-            
+            _conterLabel.WithLabels(("my_label", "my_label2")).Inc();
             return Ok();
         }
     }
--- HttpRequestDurations/WebHttpRequestDurations_3.1/Controllers/HistogramController.cs
+++ HttpRequestDurations/WebHttpRequestDurations_3.1/Controllers/HistogramController.cs
@@ -12,14 +12,14 @@ namespace CoreWebWithoutExtensions_3._1.Controllers
 
         public HistogramController(IMetricFactory metricFactory)
         {
-            _histogram = metricFactory.CreateHistogram("test_hist", "help_text", ("params1"));
+            _histogram = metricFactory.CreateHistogram("test_hist", "help_text", "params1");
         }
         
         [HttpGet]
         public IActionResult Get()
         {
-            _histogram.WithLabels("test1").Observe((1));
-            _histogram.WithLabels("test2").Observe((2));
+            _histogram.WithLabels("test1").Observe(1);
+            _histogram.WithLabels("test2").Observe(2);
             return Ok();
         }
     }
