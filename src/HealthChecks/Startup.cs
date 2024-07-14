34680f6f09881e03a220693e41904836662c6d61
Serge K <cmt@phnx47.net>
2024-07-14 13:47:25 +0700

    feat: use di in hc

--- src/HealthChecks/Startup.cs
+++ src/HealthChecks/Startup.cs
@@ -8,6 +8,7 @@ using Microsoft.Extensions.Diagnostics.HealthChecks;
 using Prometheus.Client;
 using Prometheus.Client.AspNetCore;
 using Prometheus.Client.Collectors;
+using Prometheus.Client.DependencyInjection;
 using Prometheus.Client.HealthChecks;
 
 namespace HealthChecks;
@@ -21,12 +22,11 @@ public class Startup
 
     public IConfiguration Configuration { get; }
 
-    // This method gets called by the runtime. Use this method to add services to the container.
     public void ConfigureServices(IServiceCollection services)
     {
         services.AddControllers();
-        services.AddSingleton<ICollectorRegistry, CollectorRegistry>();
-        services.AddSingleton<IMetricFactory, MetricFactory>();
+        services.AddMetricFactory();
+
         services
             .AddHealthChecks()
             .AddUrlGroup(new Uri("https://google.com"), "google", HealthStatus.Unhealthy)
@@ -34,7 +34,6 @@ public class Startup
         services.AddPrometheusHealthCheckPublisher();
     }
 
-    // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
     public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
     {
         app.UseRouting();
@@ -46,7 +45,7 @@ public class Startup
 
         app.UseHealthChecks("/hc", new HealthCheckOptions
         {
-            Predicate = r => true
+            Predicate = _ => true
         });
 
         app.UsePrometheusServer();
